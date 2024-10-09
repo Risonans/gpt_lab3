c68c082e9c2d524b14ab8bb999cc7557548e35c9
Nick271618 <nick.costareff@yandex.ru>
2024-10-09 23:14:28 +0500

    23.14

---
+++ src/js/ui/templates/blogTemplate.js
@@ -0,0 +1,13 @@
+export const left_partTemplate = ({}) => {
+    const template = `
+    <div class="blog_text_1">
+        <div class="blog_articles_data">Сент 24, 2021</div>
+        <h4 class="blog_articles_title_1">GPT-3 и OpenAI - это будущее. Давайте разберемся, как это?</h4>
+        <nav class="blog_articles_nav_1">
+            <ul class="blog_articles_link_1">
+                <li><a href="#!">Читать полную статью</a></li>
+            </ul>
+        </nav>
+    </div>
+    `
+}
