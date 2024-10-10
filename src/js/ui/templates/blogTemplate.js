d6c781c0e6d5e35bd4aeb638bce1179448624658
Nick271618 <nick.costareff@yandex.ru>
2024-10-11 00:40:01 +0500

    0.39

--- src/js/ui/templates/blogTemplate.js
+++ src/js/ui/templates/blogTemplate.js
@@ -1,51 +1,39 @@
 export const createBlog_card_1_Template = ({ src, data, title_1, button: {href, articles} }) => {
   return  `
-    <section class="con_1">
-      <div class="blog_articles_first">
-        <img src="${src}" class="blog_articles_img_first">
-        <div class="blog_text_1">
-          <div class="blog_articles_data">${data}</div>
-          <h4 class="blog_articles_title_1">${title_1}</h4>
-          <nav class="blog_articles_nav_1">
-            <ul class="blog_articles_link_1">
-              <li><a href="${href}">${articles}</a></li>
-            </ul>
-          </nav>
-        </div>
+    
+    <div class="blog_articles_first">
+      <img src="${src}" class="blog_articles_img_first">
+      <div class="blog_text_1">
+        <div class="blog_articles_data">${data}</div>
+        <h4 class="blog_articles_title_1">${title_1}</h4>
+        <nav class="blog_articles_nav_1">
+          <ul class="blog_articles_link_1">
+            <li><a href="${href}">${articles}</a></li>
+          </ul>
+        </nav>
       </div>
-    </section>
+    </div>
+    
   `;
 };
 
 export const createBlog_card_2_Template = ({ src, data, title_1, button: {href, articles} }) => {
   return  `
-    <section class="con_2">
-      <div class="blog_articles">
-        <img src="${src}" class="blog_articles_img">
-        <div class="blog_text">
-          <div class="blog_articles_data">${data}</div>
-          <h5 class="blog_articles_title">${title_1}</h5>
-          <nav class="blog_articles_nav">
-            <ul class="blog_articles_link">
-              <li><a href="${href}">${articles}</a></li>
-            </ul>
-          </nav>
-        </div>
+    
+    <div class="blog_articles">
+      <img src="${src}" class="blog_articles_img">
+      <div class="blog_text">
+        <div class="blog_articles_data">${data}</div>
+        <h5 class="blog_articles_title">${title_1}</h5>
+        <nav class="blog_articles_nav">
+          <ul class="blog_articles_link">
+            <li><a href="${href}">${articles}</a></li>
+          </ul>
+        </nav>
       </div>
-
-      <div class="blog_articles">
-        <img src="${src}" class="blog_articles_img">
-        <div class="blog_text">
-          <div class="blog_articles_data">${data}</div>
-          <h5 class="blog_articles_title">${title_1}</h5>
-          <nav class="blog_articles_nav">
-            <ul class="blog_articles_link">
-              <li><a href="${href}">${articles}</a></li>
-            </ul>
-          </nav>
-        </div>
-      </div>
-      </section>
+    </div>
+    
+      
   `;
 };
 
@@ -101,19 +89,15 @@ export const blogTemplate = ({
   const blog_card_5_Template = createBlog_card_2_Template(blog_card_5_Data);
 
   const resultTemplate = `
-        <div class="blog_articles_first">
+        <div class="con_1">
            ${blog_card_1_Template}
         </div>
-        <div class="blog_articles">
+        <div class="con_2">
            ${blog_card_2_Template}
-        </div>
-        <div class="blog_articles">
            ${blog_card_3_Template}
         </div>
-        <div class="blog_articles">
+        <div class="con_3">
            ${blog_card_4_Template}
-        </div>
-        <div class="blog_articles">
            ${blog_card_5_Template}
         </div>
     `;
