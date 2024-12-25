749e5dbde1f533ddb07626cacb2cb533cf0bce65
Nick271618 <nick.costareff@yandex.ru>
2024-12-26 00:05:40 +0500

    0,05
    End...

--- src/js/mockData/footer2Data.js
+++ src/js/mockData/footer2Data.js
@@ -1,51 +1,54 @@
-const footer2Data = [
-    {
-        /* column: 1, */
-        items: [
-            {
-                src: "./assets/img/logo.svg",
-                alt: "Логотип",
-                type: "street",
-                title: "ул. Профессора Поздеева, 13, к.Г, Пермь, Пермский край, 614013 Все права защищены",
-            },
-        ],
-    },
-    {
-        /* column: 2, */
-        items: [
-            { title: "Ссылки" },
-            { title: "Оверонны" },
-            { title: "Соц. сети" },
-            { title: "Счетчики" },
-            { title: "Контакты" },
-        ],
-    },
-    {
-        /* column: 3, */
-        items: [
-            { title: "Компания" },
-            { title: "Условия пользования" },
-            { title: "Перс. данные" },
-            { title: "Контакты" },
-        ],
-    },
-    {
-        /* column: 4, */
-        items: [
-            {
-                type: "street_contacts",
-                title: "ул. Профессора Поздеева, 13, к.Г, Пермь",
-            },
-            {
-                type: "number",
-                title: "+7 (342) 2-198-520",
-            },
-            {
-                type: "pstu_ru",
-                title: "info@pstu.ru",
-            },
-        ],
-    },
-];
+export const collum1 = {
+    items: [
+        {
+            src: "./assets/img/logo.svg",
+            alt: "Логотип",
+            type: "street",
+            title: "ул. Профессора Поздеева, 13, к.Г, Пермь, Пермский край, 614013 Все права защищены",
+        },
+    ],
+};
 
-export default footer2Data;
+export const collum2 = {
+    items: [
+        { title: "Ссылки" },
+        { title: "Оверонны" },
+        { title: "Соц. сети" },
+        { title: "Счетчики" },
+        { title: "Контакты" },
+    ],
+};
+
+export const collum3 = {
+    items: [
+        { title: "Компания" },
+        { title: "Условия пользования" },
+        { title: "Перс. данные" },
+        { title: "Контакты" },
+    ],
+};
+
+export const collum4 = {
+    items: [
+        {
+            type: "street_contacts",
+            title: "ул. Профессора Поздеева, 13, к.Г, Пермь",
+        },
+        {
+            type: "number",
+            title: "+7 (342) 2-198-520",
+        },
+        {
+            type: "pstu_ru",
+            title: "info@pstu.ru",
+        },
+    ],
+};
+
+// Сбор данных в общий объект footer2Data
+export const footer2Data = {
+    collum1,
+    collum2,
+    collum3,
+    collum4,
+};
