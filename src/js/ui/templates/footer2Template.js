d0696fa9c8b97032c276b7af88d54598cdd27b60
Nick271618 <nick.costareff@yandex.ru>
2024-12-25 20:30:45 +0500

    20,30
    ЛР3 завершена

--- src/js/ui/templates/footer2Template.js
+++ src/js/ui/templates/footer2Template.js
@@ -1,67 +1,67 @@
-export const createCollum1Template = ({ items }) => {
-    return `
-        <div class="Footer_Footer_png">
-            ${items
-                .map(
-                    ({ src, alt, type, title }) => `
-                    <img src="${src}" alt="${alt}" />
-                    <p class="${type}">${title}</p>
-                `
-                )
-                .join('')}
-        </div>
-    `;
+export const createCollumTemplate = (column) => {
+    const { items } = column;
+    switch (column.column) {
+        case 1:
+            return `
+                <div class="Footer_Footer_png">
+                    ${items
+                        .map(
+                            ({ src, alt, type, title }) => `
+                            <img src="${src}" alt="${alt}" />
+                            <p class="${type}">${title}</p>
+                        `
+                        )
+                        .join('')}
+                </div>
+            `;
+        case 2:
+            return `
+                <nav class="yadurak">
+                    <h11>${items[0]?.title || ''}</h11>
+                    <ul class="Footer_Footer_links">
+                        ${items
+                            .slice(1)
+                            .map(({ title }) => `<li><a href="#!">${title}</a></li>`)
+                            .join('')}
+                    </ul>
+                </nav>
+            `;
+        case 3:
+            return `
+                <nav class="nedurak">
+                    <h12>${items[0]?.title || ''}</h12>
+                    <ul class="Footer_Footer_company">
+                        ${items
+                            .slice(1)
+                            .map(({ title }) => `<li><a href="#!">${title}</a></li>`)
+                            .join('')}
+                    </ul>
+                </nav>
+            `;
+        case 4:
+            return `
+                <div class="Footer_Footer_contacts">
+                    <h13>${items[0]?.title || ''}</h13>
+                    ${items
+                        .slice(1)
+                        .map(
+                            ({ type, title }) => `
+                            <p class="${type}">${title}</p>
+                        `
+                        )
+                        .join('')}
+                </div>
+            `;
+        default:
+            return '';
+    }
 };
 
-export const createCollum2Template = ({ items }) => {
-    return `
-        <nav class="yadurak">
-            <h11>${items[0]?.title || ''}</h11>
-            <ul class="Footer_Footer_links">
-                ${items
-                    .slice(1)
-                    .map(({ title }) => `<li><a href="#!">${title}</a></li>`)
-                    .join('')}
-            </ul>
-        </nav>
-    `;
-};
-
-export const createCollum3Template = ({ items }) => {
-    return `
-        <nav class="nedurak">
-            <h12>${items[0]?.title || ''}</h12>
-            <ul class="Footer_Footer_company">
-                ${items
-                    .slice(1)
-                    .map(({ title }) => `<li><a href="#!">${title}</a></li>`)
-                    .join('')}
-            </ul>
-        </nav>
-    `;
-};
-
-export const createCollum4Template = ({ items }) => {
-    return `
-        <div class="Footer_Footer_contacts">
-            <h13>${items[0]?.title || ''}</h13>
-            ${items
-                .slice(1)
-                .map(
-                    ({ type, title }) => `
-                    <p class="${type}">${title}</p>
-                `
-                )
-                .join('')}
-        </div>
-    `;
-};
-
-export const footer2Template = ({ collum1, collum2, collum3, collum4 }) => {
-    const collum1Template = createCollum1Template(collum1);
-    const collum2Template = createCollum2Template(collum2);
-    const collum3Template = createCollum3Template(collum3);
-    const collum4Template = createCollum4Template(collum4);
+export const footer2Template = (footer2Data) => {
+    const collum1Template = createCollumTemplate(footer2Data[0]);
+    const collum2Template = createCollumTemplate(footer2Data[1]);
+    const collum3Template = createCollumTemplate(footer2Data[2]);
+    const collum4Template = createCollumTemplate(footer2Data[3]);
 
     const resultTemplate = `
         <div class="Footer_Footer">
